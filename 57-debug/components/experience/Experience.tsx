992d953041a87b23e3b445460eba9b924149895b
jungminyoo <yoojm4718@gmail.com>
2026-02-03 14:48:29 +0900

    learned R3F debug

---
+++ 57-debug/components/experience/Experience.tsx
@@ -0,0 +1,56 @@
+import { OrbitControls } from "@react-three/drei";
+import { button, useControls } from "leva";
+import { Perf } from "r3f-perf";
+
+export default function Experience() {
+  const { perfVisible } = useControls("Perf", {
+    perfVisible: false,
+  });
+
+  const { position, color, visible } = useControls("Sphere", {
+    position: {
+      value: { x: -2, y: 0 },
+      step: 0.01,
+      joystick: "invertY",
+    },
+    color: "#ff0000",
+    visible: true,
+    // myInterval: {
+    //   min: 0,
+    //   max: 10,
+    //   value: [4, 5],
+    // },
+    // clickMe: button(() => {
+    //   console.log("hello");
+    // }),
+    // choice: {
+    //   options: ["a", "b", "c"],
+    // },
+  });
+
+  return (
+    <>
+      {perfVisible && <Perf position="top-left" />}
+
+      <OrbitControls makeDefault />
+
+      <directionalLight position={[1, 2, 3]} intensity={4.5} />
+      <ambientLight intensity={1.5} />
+
+      <mesh position={[position.x, position.y, 0]} visible={visible}>
+        <sphereGeometry />
+        <meshStandardMaterial color={color} />
+      </mesh>
+
+      <mesh position-x={2} scale={1.5}>
+        <boxGeometry />
+        <meshStandardMaterial color="mediumpurple" />
+      </mesh>
+
+      <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+        <planeGeometry />
+        <meshStandardMaterial color="greenyellow" />
+      </mesh>
+    </>
+  );
+}
---
+++ 57-debug/components/experience/ExperienceCanvas.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import * as THREE from "three";
+import { Canvas } from "@react-three/fiber";
+import Experience from "./Experience";
+import { Leva } from "leva";
+
+export default function ExperienceCanvas() {
+  return (
+    <>
+      <Leva collapsed />
+      <Canvas camera={{ fov: 45, near: 0.1, far: 200, position: [3, 2, 6] }}>
+        <Experience />
+      </Canvas>
+    </>
+  );
+}
